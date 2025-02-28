dc1c1c07ea1217b71f073d5c836187a923d2f37e
WAL <91624443+waldivamweb21@users.noreply.github.com>
2025-02-28 20:57:52 -0300

    Update settings.py
    seguranca no settings.

--- fundamentos/setup/settings.py
+++ fundamentos/setup/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ygz)-1dslnmk#52(it%ufywd%-rb6)z2rwa=m4y8wq+810xz&6'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
